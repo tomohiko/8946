8fa81263cfcc63f6bf22ed2ad50103f91bc43b21
tomohiko <tomohiko@swdis.net>
2015-02-11 20:24:01 +0900

    Create hira.py

---
+++ hira.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+import hashlib
+
+start =  ord(u'あ')
+end =  ord(u'ん')
+
+hira = []
+print "Create hiragana"
+for i in range(start, end+1, 1):
+        hira.append(unichr(i).encode('utf-8'))
+
+num = len(hira)
+for i4 in range(num):
+    for i3 in range(num):
+        for i2 in range(num):
+                for i1 in range(num):
+                    msg = hira[i1] + hira[i2] + hira[i3] + hira[i4]
+                            print msg,
+                            print hashlib.md5(msg).hexdigest()
